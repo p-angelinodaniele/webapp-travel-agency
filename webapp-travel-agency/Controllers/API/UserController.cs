050bf29b6cfaec0b1c302ec0e70ce0df539a73ea
P.angelino <p.angelinodaniele@gmail.com>
2022-05-23 16:12:43 +0200

    Milestone 2

---
+++ webapp-travel-agency/Controllers/API/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapp_travel_agency.DataBase;
+using webapp_travel_agency.Models;
+
+namespace webapp_travel_agency.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<PacchettoViaggio> listaPacchetti = new List<PacchettoViaggio>();
+            using (AgenziaContext db = new AgenziaContext())
+            {
+                listaPacchetti = db.PacchettoViaggio.ToList<PacchettoViaggio>();
+            }
+            return Ok(listaPacchetti);
+        }
+    }
+}
