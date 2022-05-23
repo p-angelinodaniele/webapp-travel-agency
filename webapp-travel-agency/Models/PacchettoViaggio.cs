cfc47ae91db31f9ee91165bdbe775f25105471bf
P.angelino <p.angelinodaniele@gmail.com>
2022-05-23 09:58:27 +0200

    classe pacchettoViaggi

--- webapp-travel-agency/Models/PacchettoViaggio.cs
+++ webapp-travel-agency/Models/PacchettoViaggio.cs
@@ -2,6 +2,30 @@
 {
     public class PacchettoViaggio
     {
+        
+        public int Id { get; set; }
+        public string Titolo { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Prezzo { get; set; }
+        public string Destinazioni { get; set; }
+        public string Giorni { get; set; }
 
+
+
+
+        public PacchettoViaggio()
+        {
+
+        }
+        public PacchettoViaggio(string titolo, string description, string image, double prezzo, string destinazioni, string giorni)
+        {
+            Titolo = titolo;
+            Description = description;
+            Image = image;
+            Prezzo = prezzo;
+            Destinazioni = destinazioni;
+            Giorni = giorni;
+        }
     }
 }
