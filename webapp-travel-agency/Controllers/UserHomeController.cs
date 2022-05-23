d85280b76a37b4d2db201b665edef08b5a5e5faf
P.angelino <p.angelinodaniele@gmail.com>
2022-05-23 17:10:33 +0200

    Milestone 2

---
+++ webapp-travel-agency/Controllers/UserHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_travel_agency.Controllers
+{
+    public class UserHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
