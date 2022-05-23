94e8e7e6c3290c67213a773e47e647ad4f725a93
P.angelino <p.angelinodaniele@gmail.com>
2022-05-23 10:20:54 +0200

    migrations

---
+++ webapp-travel-agency/Migrations/20220523081509_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapp_travel_agency.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PacchettoViaggio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titolo = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Description = table.Column<string>(type: "text", maxLength: 100, nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prezzo = table.Column<double>(type: "float", nullable: false),
+                    Destinazioni = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Giorni = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PacchettoViaggio", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PacchettoViaggio");
+        }
+    }
+}
