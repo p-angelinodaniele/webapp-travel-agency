eaed1beafb6e45ca8542fff7cac6c80fabac1275
P.angelino <p.angelinodaniele@gmail.com>
2022-05-23 10:14:51 +0200

    aggiunta dbContext

---
+++ webapp-travel-agency/DataBase/AgenziaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapp_travel_agency.Models;
+
+
+namespace webapp_travel_agency.DataBase
+{
+    public class AgenziaContext : DbContext
+    {
+        public DbSet<PacchettoViaggio> PacchettoViaggio { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=Agenzia;Integrated Security=True");
+        }
+    }
+}
--- webapp-travel-agency/Models/PacchettoViaggio.cs
+++ webapp-travel-agency/Models/PacchettoViaggio.cs
@@ -1,14 +1,30 @@
-﻿namespace webapp_travel_agency.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapp_travel_agency.Models
 {
     public class PacchettoViaggio
     {
-        
+        [Key]
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
+        [StringLength(20, ErrorMessage = "Il titolo non può avere più di 20 caratteri")]
         public string Titolo { get; set; }
+
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
+        [StringLength(100, ErrorMessage = "La descrizione non può avere più di 100 caratteri")]
+        [Column(TypeName = "text")]
         public string Description { get; set; }
         public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
         public double Prezzo { get; set; }
+
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
         public string Destinazioni { get; set; }
+
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
         public string Giorni { get; set; }
 
 
